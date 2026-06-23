775dd83a654232bf796562e89ca5b3d2a290f752
rahim989 <abdurrahim17@cse.pstu.ac.bd>
2026-06-23 19:00:32 +0600

    fix: static export for netlify

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // এই লাইনটি আপনার প্রোজেক্টকে Netlify-এর জন্য রেডি করবে
   typescript: {
-    // বিল্ড করার সময় টাইপস্ক্রিপ্টের ছোটখাটো ভুল বা এরর ইগনোর করবে
     ignoreBuildErrors: true,
   },
   eslint: {
-    // বিল্ড করার সময় এসলিন্ট (ESLint) এরর ইগনোর করবে
     ignoreDuringBuilds: true,
   },
 };
